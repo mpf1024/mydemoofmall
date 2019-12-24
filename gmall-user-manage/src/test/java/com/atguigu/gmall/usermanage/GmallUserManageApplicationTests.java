d6adfbbc60af704754eaee3a0a5cce1705e63b38
mpf <mpingfan@foxmail.com>
2019-12-24 21:04:07 +0800

    userInfo的简单CRUD

---
+++ gmall-user-manage/src/test/java/com/atguigu/gmall/usermanage/GmallUserManageApplicationTests.java
@@ -0,0 +1,16 @@
+package com.atguigu.gmall.usermanage;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.sql.SQLException;
+
+@SpringBootTest
+class GmallUserManageApplicationTests {
+
+    @Test
+    public void contextLoads() throws SQLException {
+
+    }
+
+}
